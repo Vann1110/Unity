6b071db9bd4b38bcf88768195028fd506f24346a
Vann <maka3810@gmail.com>
2021-04-19 18:05:46 +0800

    FairyGUI [新增] 資源發布、動態載入

---
+++ Assets/Sample/FairyGUI/Scripts/FairyGUITest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class FairyGUITest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GRoot.inst.SetContentScaleFactor(800, 600);
+        UIPackage.AddPackage("FGUI/Package1");
+        GComponent component = UIPackage.CreateObject("Package1", "Component2").asCom;
+        GRoot.inst.AddChild(component);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
