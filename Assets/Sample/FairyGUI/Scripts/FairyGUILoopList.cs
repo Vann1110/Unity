4b7f0bbdfe9a81e759fc9458904765aebb7213db
Vann <maka3810@gmail.com>
2021-04-21 17:36:08 +0800

    FairyGUI [新增] 選單縮放

--- Assets/Sample/FairyGUI/Scripts/FairyGUILoopList.cs
+++ Assets/Sample/FairyGUI/Scripts/FairyGUILoopList.cs
@@ -15,12 +15,36 @@ public class FairyGUILoopList : MonoBehaviour
         this.list.SetVirtualAndLoop();
         this.list.itemRenderer = this.renderListItem;
         this.list.numItems = 5;
+        this.list.scrollPane.onScroll.Add(this.zoomEffect);
+        this.zoomEffect();
+    }
+
+    private void zoomEffect()
+    {
+        float listCenter = this.list.scrollPane.posX + this.list.viewWidth / 2;
+        // 當前渲染之物件
+        for (int i = 0; i < list.numChildren; i++)
+        {
+            GObject item = this.list.GetChildAt(i);
+            float itemCenter = item.x + item.width / 2;
+            float itemWidth = item.width;
+            float dist = Mathf.Abs(listCenter - itemCenter);
+            if (dist < itemWidth)
+            {
+                float distRange = 1 + (1 - dist / itemWidth) * 0.2f;
+                item.SetScale(distRange, distRange);
+            }
+            else
+            {
+                item.SetScale(1, 1);
+            }
+        }
     }
 
     private void renderListItem(int index, GObject obj)
     {
         GButton btn = obj.asButton;
-        Debug.Log(index);
+        btn.SetPivot(0.5f, 0.5f);
         btn.icon = UIPackage.GetItemURL("LoopListPack", "n" + (index + 1));
     }
 }
