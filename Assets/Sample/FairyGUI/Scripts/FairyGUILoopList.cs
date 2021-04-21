6f8f9f35c8ff2b7644b2795056d975b62921e259
Vann <maka3810@gmail.com>
2021-04-21 16:52:25 +0800

    FairyGUI [新增] 循環列表

---
+++ Assets/Sample/FairyGUI/Scripts/FairyGUILoopList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class FairyGUILoopList : MonoBehaviour
+{
+    private GComponent mainUI;
+    private GList list;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mainUI = this.GetComponent<UIPanel>().ui;
+        this.list = this.mainUI.GetChild("n0").asList;
+        this.list.SetVirtualAndLoop();
+        this.list.itemRenderer = this.renderListItem;
+        this.list.numItems = 5;
+    }
+
+    private void renderListItem(int index, GObject obj)
+    {
+        GButton btn = obj.asButton;
+        Debug.Log(index);
+        btn.icon = UIPackage.GetItemURL("LoopListPack", "n" + (index + 1));
+    }
+}
