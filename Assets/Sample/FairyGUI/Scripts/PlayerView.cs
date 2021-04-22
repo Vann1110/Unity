73bd7f7d24b7add8be1e8465e5f9add418432923
Vann <maka3810@gmail.com>
2021-04-22 18:49:36 +0800

    FairyGUI [新增] 人物视窗弹窗

---
+++ Assets/Sample/FairyGUI/Scripts/PlayerView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class PlayerView : MonoBehaviour
+{
+    private GComponent mainUI;
+    private PlayerWindow playerWindow;
+    public GameObject npc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mainUI = this.GetComponent<UIPanel>().ui;
+        this.playerWindow = new PlayerWindow(this.npc);
+        this.mainUI.GetChild("n0").onClick.Add(() => { this.playerWindow.Show(); });
+    }
+}
---
+++ Assets/Sample/FairyGUI/Scripts/PlayerWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class PlayerWindow : Window
+{
+    private GameObject npc;
+
+    public PlayerWindow(GameObject npc)
+    {
+        this.npc = npc;
+    }
+
+    protected override void OnInit()
+    {
+        this.contentPane = UIPackage.CreateObject("07-PlayerView", "PlayerWindow").asCom;
+        GGraph loader = this.contentPane.GetChild("n2").asGraph;
+        RenderTexture renderTexture = Resources.Load<RenderTexture>("FGUI/07-PlayerView/PlayerRT");
+        Material mat = Resources.Load<Material>("FGUI/07-PlayerView/PlayerMat");
+        Image img = new Image();
+        img.texture = new NTexture(renderTexture);
+        img.material = mat;
+        loader.SetNativeObject(img);
+        this.contentPane.GetChild("n3").onClick.Add(() => { this.rotateLeft(); });
+        this.contentPane.GetChild("n4").onClick.Add(() => { this.rotateRight();});
+    }
+
+    private void rotateLeft()
+    {
+        this.npc.transform.Rotate(Vector3.up * 30, Space.World);
+    }
+    private void rotateRight()
+    {
+        this.npc.transform.Rotate(Vector3.up * -30, Space.World);
+    }
+}
