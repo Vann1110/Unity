3218d992da45045e5093a928932af1fdf3c042f7
Vann <maka3810@gmail.com>
2021-04-23 18:51:42 +0800

    FairyGUI [新增] 背包範例

---
+++ Assets/Sample/FairyGUI/Scripts/BagManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class BagManager : MonoBehaviour
+{
+    private GComponent mainUI;
+    private GButton playerView;
+    private BagListWindow bagListWindow;
+    // private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mainUI = this.GetComponent<UIPanel>().ui;
+        this.playerView = this.mainUI.GetChild("playerView").asButton;
+        this.playerView.onClick.Add(this.userItem);
+        this.bagListWindow = new BagListWindow(this.playerView);
+        this.bagListWindow.SetXY(121, 63);
+        this.mainUI.GetChild("bagButton").onClick.Add(() =>
+        {
+            this.bagListWindow.Show();
+        });
+    }
+
+    private void userItem()
+    {
+        this.playerView.icon = null;
+        this.playerView.title = "空白";
+    }
+}
---
+++ Assets/Sample/FairyGUI/Scripts/BagWindow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class BagListWindow : Window
+{
+    private GList list;
+    private GButton playerView;
+
+    public BagListWindow(GButton targetBtn)
+    {
+        this.playerView = targetBtn;
+    }
+
+    protected override void OnInit()
+    {
+        this.contentPane = UIPackage.CreateObject("08-Bag", "BagWindow").asCom;
+        this.list = this.contentPane.GetChild("itemList").asList;
+        this.list.itemRenderer = this.renderItemList;
+        this.list.numItems = 20;
+        for (int i = 0; i < 10; i++)
+        {
+            GButton button = this.list.GetChildAt(i).asButton;
+            button.onClick.Add(() =>
+            {
+                this.itemCLick(button);
+            });
+        }
+    }
+
+    private void renderItemList(int index, GObject obj)
+    {
+        GButton button = obj.asButton;
+        button.icon = UIPackage.GetItemURL("08-Bag", "i" + index);
+        button.title = index.ToString();
+    }
+
+    private void itemCLick(GButton btn)
+    {
+        this.playerView.title = btn.title;
+        this.playerView.icon = btn.icon;
+    }
+}
